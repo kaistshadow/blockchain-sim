641e8b9f95019823e01da45133b0584477649008
Yonggon Kim <ygnkim@kaist.ac.kr>
2019-03-30 16:32:01 +0900

    add libBLEEP ArgsManager. implement node, client example & test configuration.

---
+++ plugins/blockchain-peer/libBLEEP-1.0/utility/ArgsManager.h
@@ -0,0 +1,70 @@
+#ifndef ARGS_MANAGER_H
+#define ARGS_MANAGER_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace libBLEEP {
+
+    class ArgsManager {
+    protected:
+        std::map<std::string, std::string> mapArgs;
+        std::map<std::string, std::vector<std::string>> mapMultiArgs;
+
+
+    public:
+        std::string HelpMessage();
+
+        void ParseParameters(int argc, const char*const argv[]);
+
+        /**
+         * Return a vector of strings of the given argument
+         *
+         * @param strArg Argument to get (e.g. "-foo")
+         * @return command-line arguments
+         */
+        std::vector<std::string> GetArgs(const std::string& strArg) const;
+
+        /**
+         * Return string argument or default value
+         *
+         * @param strArg Argument to get (e.g. "-foo")
+         * @param strDefault (e.g. "1")
+         * @return command-line argument or default value
+         */
+        std::string GetArg(const std::string& strArg, const std::string& strDefault) const;
+
+        /**
+         * Return integer argument or default value
+         *
+         * @param strArg Argument to get (e.g. "-foo")
+         * @param nDefault (e.g. 1)
+         * @return command-line argument (0 if invalid number) or default value
+         */
+        int GetArg(const std::string& strArg, int nDefault) const;
+
+        /**
+         * Return boolean argument or default value
+         *
+         * @param strArg Argument to get (e.g. "-foo")
+         * @param fDefault (true or false)
+         * @return command-line argument or default value
+         */
+        bool GetBoolArg(const std::string& strArg, bool fDefault) const;
+
+        /**
+         * Return true if the given argument has been manually set
+         *
+         * @param strArg Argument to get (e.g. "-foo")
+         * @return true if the argument has been set
+         */
+        bool IsArgSet(const std::string& strArg) const;
+
+    };
+
+    extern ArgsManager gArgs;
+}
+
+
+#endif
---
+++ plugins/blockchain-peer/rc-1.0-FSM/node_examples/client.cpp
@@ -0,0 +1,53 @@
+#include "mainmodules/MainEventManager.h"
+#include "datamodules/Peer.h"
+
+#include "utility/ArgsManager.h"
+
+using namespace libBLEEP;
+
+// TODO : create randomly generate transaction, and send it to blockchain node
+
+int main(int argc, char *argv[]) {
+
+    gArgs.ParseParameters(argc, argv);
+
+    if (gArgs.IsArgSet("-?") || gArgs.IsArgSet("-h") ||  gArgs.IsArgSet("-help") || gArgs.IsArgSet("-version")) {
+        std::string strUsage = gArgs.HelpMessage();
+        std::cout << strUsage << "\n";
+        return 0;
+    }
+
+    /* allocate mainEventManager */
+    // MainEventManager mainEventManager("143.248.38.189");
+    MainEventManager mainEventManager(gArgs.GetArg("-id", "noid"));
+
+    /* connect to peer */
+    for (auto neighborPeerId : gArgs.GetArgs("-connect"))
+        mainEventManager.AsyncConnectPeer(PeerId(neighborPeerId));
+
+    while(true) {
+        mainEventManager.Wait(); // main event loop (wait for next event)
+        
+        switch (mainEventManager.GetEventType()) {
+        case AsyncEventEnum::none:
+            std::cout << "invalid event is triggered. " << "\n";
+            exit(-1);
+        case AsyncEventEnum::CompleteAsyncConnectPeer:
+            {
+                std::cout << "event for connection complete for peer. " << "\n";
+                PeerId peerId = mainEventManager.GetAsyncEventDataManager().GetConnectedPeerId();
+                std::cout << "connected peerId : " << peerId.GetId() << "\n";
+                // mainEventManager.SendMessage(message);
+                break;
+            }
+        case AsyncEventEnum::ErrorAsyncConnectPeer:
+            {
+                std::cout << "AsyncConnectPeer got error(" << mainEventManager.GetAsyncEventDataManager().GetError() << ")" << "\n";
+                // try again with timer
+                PeerId peerId = mainEventManager.GetAsyncEventDataManager().GetRefusedPeerId();                
+                mainEventManager.AsyncConnectPeer(peerId, 10);
+                break;
+            }
+        }
+    }
+}

14e587d41b599db3ef71bc9ffd5cae6c0ffe9e03
Yonggon Kim <ygnkim@kaist.ac.kr>
2019-04-09 17:30:58 +0900

    update setup.py shadow-bitcoin/setup.py

--- setup.py
+++ setup.py
@@ -47,62 +47,14 @@ def prepare_nodejs():
     os.system("cd %s; npm install @maxmind/geoip2-node" % nodejs_serv_path)
 
 
-def setup_multiple_node_xml(node_num):
-    baseline_xml = "plugins/shadow-plugin-bitcoin/resource/example.xml"
-    new_xml = "plugins/shadow-plugin-bitcoin/resource/example_multiple_generated.xml"
-
-    parser = ET.XMLParser(remove_blank_text=True, strip_cdata=False)
-
-    tree = ET.parse(baseline_xml, parser)
-
-    shadow = tree.getroot()
-
-    for node in shadow.findall('node'):
-        shadow.remove(node)
-
-    # node = ET.SubElement(shadow, "node", id="bcdnode0")
-    # ET.SubElement(node, "application", plugin="bitcoind", time="5", arguments="-datadir=./data/bcdnode0 -debug -printtoconsole -disablewallet=1 -server=0")
-
-    for i in range(node_num):
-        node_id = "bcdnode%d" % (i)
-
-        node = ET.SubElement(shadow, "node", id=node_id)            
-        time = str(5 + i/100)
-        if i % 2 == 0:
-            ET.SubElement(node, "application", plugin="bitcoind", time=time, arguments="-datadir=./data/bcdnode%d -debug -printtoconsole -listen -connect=bcdnode%d -disablewallet=1 -server=0"%(i, i+1))
-        else:
-            ET.SubElement(node, "application", plugin="bitcoind", time=time, arguments="-datadir=./data/bcdnode%d -debug -printtoconsole -listen -connect=bcdnode%d -disablewallet=1 -server=0"%(i, i-1))
-
-    tree.write(new_xml, pretty_print=True)
-    
-def setup_injector_plugin():
-    injector_plugin_path = "./injectors/shadow-injector-bitcoin"
-    os.system("cd %s; mkdir build; cd build; cmake ../ -DSHADOW=ON; make; make install" % injector_plugin_path)
-    
-def run_shadow_bitcoin_injector_example():
-    run_path = "plugins/shadow-plugin-bitcoin/run"
-    os.system("mkdir -p %s" % run_path)
-    os.system("mkdir -p %s/data/bcdnode1; mkdir -p %s/data/bcdnode2;" % (run_path, run_path))
-    os.system("cd %s; shadow ../resource/example_injector.xml" % run_path)
-    
-def run_shadow_bitcoin_multiple_node(node_num, worker_num):
-    run_path = "plugins/shadow-plugin-bitcoin/run"
-    os.system("rm -rf %s/data/bcdnode*" % run_path)
-    for i in range(node_num):
-        os.system("mkdir -p %s/data/bcdnode%d" % (run_path, i))
-    os.system("mkdir %s/shadow.result" % run_path)
-    os.system("cd %s; shadow -w %d ../resource/%s | tee shadow.result/shadow_%d_%d.result" % (run_path, worker_num, "example_multiple_generated.xml", node_num, worker_num))
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script for installation and simulation')
-    parser.add_argument("--install", action="store_true", help="Install the shadow simulator and bitcoin-plugin")
-    parser.add_argument("--injector", action="store_true", help="Run bitcoin-plugin and injector-plugin")
+    parser.add_argument("--install", action="store_true", help="Install the shadow simulator and BLEEP")
     parser.add_argument("--test", action="store_true", help="Run tests")
     parser.add_argument("--debug", action="store_true", help="Include debug symbols for shadow")
 
     args = parser.parse_args()
     OPT_INSTALL = args.install
-    OPT_INJECTOR = args.injector
     OPT_TEST = args.test
     OPT_DEBUG = args.debug
     
@@ -112,22 +64,14 @@ if __name__ == '__main__':
     
     if OPT_INSTALL:
         prepare_shadow()
-        # prepare_nodejs()
+        prepare_nodejs()
         os.system("mkdir build; cd build; cmake %s ../; make; make install" % cmake_debug_opt)
         os.system("echo 'export PATH=$PATH:%s' >> ~/.bashrc && . ~/.bashrc" % os.path.expanduser("~/.shadow/bin"))
         print "After installing the shadow, execute following commands on your bash. (type without dollor sign)"
         print "$ source ~/.bashrc"
 
-    if OPT_INJECTOR:
-        setup_injector_plugin()
-        run_shadow_bitcoin_injector_example()
-
     if OPT_TEST:
         os.system("cd build; make test")
 
-
-    ### Following commands are example for the experiment of multiple bitcoin nodes.
-    # setup_multiple_node_xml(100)
-    # run_shadow_bitcoin_multiple_node(100, 1)
 
     

ada92243eaccce3e206735a63539ca5877f37e8f
Yonggon Kim <ygnkim@kaist.ac.kr>
2020-03-16 16:45:31 +0900

    remove redundant code from rust's 1_hello test

--- tests/regtest/rust/1_hello/src/lib.rs
+++ tests/regtest/rust/1_hello/src/lib.rs
@@ -1,6 +1,7 @@
 #[no_mangle]
-pub extern fn main() {
+pub extern "C" fn main() -> i32 {
     println!("main in lib.rs called\n");
     // rust does not use implicit return code on the main, but Shadow requires return code for the plugin's main function.
-    std::process::exit(0);
+
+    0 // return value
 }

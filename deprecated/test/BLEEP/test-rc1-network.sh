62fdd8ff9c0597931ee8c190bcadcbef1a6a4c27
joonhyuk96sj <joonhyuk96sj@kaist.ac.kr>
2020-04-12 15:26:44 +0900

    Check dir clear

--- deprecated/test/BLEEP/test-rc1-network.sh
+++ deprecated/test/BLEEP/test-rc1-network.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 if [ $# -eq 1 ]; then
-    python test-rc1-network.py --nodenum $1
-    #python print_log.py $1 rc1-datadir PEER
-    python invariant_network_graph.py $1
+    python python-script/test-rc1-network.py --nodenum $1
+    python python-script/network_graph.py $1
 
 elif [ $# -eq 2 ]; then
     n=`expr $1 + 1`
-    python test-rc1-network.py --nodenum $1 $2
-    #python print_log.py $n rc1-datadir PEER
-    python invariant_network_graph.py $n
-    python invariant_blockchain_graph.py $1 rc1-datadir
+    python python-script/test-rc1-network.py --nodenum $1 $2
+    python python-script/network_graph.py $n
+    python python-script/blockchain_graph.py $1 rc1-datadir
 
 else
     echo "Invalid Arguments"

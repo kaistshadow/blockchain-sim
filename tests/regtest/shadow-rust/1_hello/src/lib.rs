1e6778a237fcda6458ce243a744ce1b7c30f82b1
Yonggon Kim <ygnkim@kaist.ac.kr>
2020-05-14 20:11:48 +0900

    debug hello-rust error (WIP)

--- tests/regtest/shadow-rust/1_hello/src/lib.rs
+++ tests/regtest/shadow-rust/1_hello/src/lib.rs
@@ -4,6 +4,7 @@ pub extern "C" fn main() -> i32 {
     // rust does not use implicit return code on the main, but Shadow requires return code for the plugin's main function.
 
     
-    std::process::exit(0)
+    return 0
+    // std::process::exit(0) // It also results in abort error on github action.
     // 0 // return value, but this results in abort error on github action.
 }

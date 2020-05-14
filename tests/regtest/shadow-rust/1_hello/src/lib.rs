aa94b53cb17c217bd9509666aa42ae93a1c7dee6
Yonggon Kim <ygnkim@kaist.ac.kr>
2020-05-14 20:19:05 +0900

    remove hello-rust test

--- tests/regtest/shadow-rust/1_hello/src/lib.rs
+++ tests/regtest/shadow-rust/1_hello/src/lib.rs
@@ -4,7 +4,7 @@ pub extern "C" fn main() -> i32 {
     // rust does not use implicit return code on the main, but Shadow requires return code for the plugin's main function.
 
     
-    return 0
-    // std::process::exit(0) // It also results in abort error on github action.
-    // 0 // return value, but this results in abort error on github action.
+    // return 0 // results in abort error on github action.
+    // std::process::exit(0) // results in abort error on github action.
+    0 // return value, but this results in abort error on github action.
 }

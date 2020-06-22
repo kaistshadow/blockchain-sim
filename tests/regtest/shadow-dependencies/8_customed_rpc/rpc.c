9e6cea5cbfae84f2d2b8c0153f63161ac3a8b146
victork86 <victor86@kaist.ac.kr>
2020-06-22 14:50:16 +0900

    add rest sample

--- tests/regtest/shadow-dependencies/8_customed_rpc/rpc.c
+++ tests/regtest/shadow-dependencies/8_customed_rpc/rpc.c
@@ -3,6 +3,7 @@
 #include <curl/curl.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <string.h>
 
 int main(void)
 {
@@ -10,6 +11,7 @@ int main(void)
 
     CURL *curl;
     CURLcode res;
+    char *host = "http://213.145.108.101:18332"; //127.0.0.1:18332 213.145.108.101:18332
 
     curl = curl_easy_init();
     struct curl_slist *headers = NULL;
@@ -17,43 +19,35 @@ int main(void)
     if (curl) {
 	const char *data ="{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"getblockchaininfo\", \"params\": [] }";
 
-
-	struct addrinfo* servinfo;
-    int n = getaddrinfo(domain.c_str(), NULL, NULL, &servinfo);
-    if (n != 0) {
-        std::cout << "Value of errno: " << errno << "\n";
-        fprintf(stderr, "getaddrinfo:%s\n", gai_strerror(n));
-        perror("getaddrinfo");
-        std::cout << "domain" << domain << "\n";
-    }
-
-
 	headers = curl_slist_append(headers, "content-type: text/plain;");
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-	// curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:11111");
-	curl_easy_setopt(curl, CURLOPT_URL, "https://bcdnode1");
+	curl_easy_setopt(curl, CURLOPT_URL, host);
 
 	curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long) strlen(data));
 	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
 
-	curl_easy_setopt(curl, CURLOPT_USERPWD,
-			 "a:1234");
-
+	curl_easy_setopt(curl, CURLOPT_USERPWD, "node1:1234");
 	curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_TRY);
-    printf("아하1\n");
-    res = curl_easy_perform(curl);
-    printf("아하2\n");
 
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+    printf("step1\n");
+    res = curl_easy_perform(curl);
+    printf("step2\n");
+
+    /* Check for errors */
+    while(res != CURLE_OK) {
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        else
+            printf("success\n");
+        sleep(1);
+    }
  
     /* always cleanup */ 
     curl_easy_cleanup(curl);  
       }
 
-    printf("아하3\n");
+    printf("step3\n");
     return 0;
 }

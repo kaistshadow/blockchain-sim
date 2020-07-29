71fdca6cb570c999a0d891b7baa469b5d42f8b06
hjkim17 <ouiqdmw17@gmail.com>
2020-07-29 09:32:13 +0900

    plugin name fix

--- tests/regtest/shadow-bitcoin/3_dev-test/make_approximate_setmining_test.py
+++ tests/regtest/shadow-bitcoin/3_dev-test/make_approximate_setmining_test.py
@@ -26,7 +26,7 @@ def setup_multiple_node_xml(node_num, simultime):
             argument = "-debug -testnet -datadir=data/bcdnode%d -port=11110 -rpcuser=a -rpcpassword=1234 -rpcport=11111 -rpcallowip=11.0.0.%d/0 -rpcbind=11.0.0.%d -addnode=11.0.0.%d:11110 -addnode=11.0.0.%d:11110" % (i, (i+1), (i+1), node_num, (i+2))
         else:
             argument = "-debug -testnet -datadir=data/bcdnode%d -port=11110 -rpcuser=a -rpcpassword=1234 -rpcport=11111 -rpcallowip=11.0.0.%d/0 -rpcbind=11.0.0.%d -addnode=11.0.0.%d:11110 -addnode=11.0.0.%d:11110" % (i, (i+1), (i+1), i, (i+2))
-        ET.SubElement(node,"application", plugin="NODE", time=time, arguments=argument)
+        ET.SubElement(node,"application", plugin="bitcoind", time=time, arguments=argument)
 
     for i in range(0, node_num):
         node_id = "client%d" % (i)

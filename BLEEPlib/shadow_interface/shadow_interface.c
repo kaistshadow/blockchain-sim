b7b6d2931a11607d52e4aefdf1c10e934a9849de
hjkim17 <ouiqdmw17@gmail.com>
2020-09-24 17:00:03 +0900

    local shadow interface function implemented

--- BLEEPlib/shadow_interface/shadow_interface.c
+++ BLEEPlib/shadow_interface/shadow_interface.c
@@ -1,5 +1,7 @@
 #include "shadow_interface.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 struct timespec clock_start;
 
@@ -74,11 +76,72 @@ int copy_dat_files(int fileno) {
 // Element 1: unique hash
 // Element 2: actual path
 // Element 3: reference count
+struct shared_file_list {
+    char* name;
+    struct shared_file_list* prev;
+    struct shared_file_list* next;
+};
+struct shared_file_list_header {
+    struct shared_file_list* head;
+    struct shared_file_list* tail;
+};
+struct shared_file_list_header shd_hdr = {NULL, NULL};
+struct shared_file_list* find_shared_file(const char * filename) {
+    struct shared_file_list* cur = shd_hdr.head;
+    while(cur) {
+        if(strcmp(filename, cur->name)==0) {
+            return cur;
+        }
+        cur = cur->next;
+    }
+    return NULL;
+}
+void add_shared_file_list(const char* filename) {
+    struct shared_file_list* sfl = (struct shared_file_list*)malloc(sizeof(struct shared_file_list));
+    int nameSize = strlen(filename);
+    sfl->name = (char*)malloc(sizeof(char)*(nameSize)+1);
+    memcpy(sfl->name, filename, nameSize);
+    sfl->name[nameSize] = 0;
+    sfl->next = NULL;
+    if (!shd_hdr.head) {
+        sfl->prev = NULL;
+        shd_hdr.head = sfl;
+    } else {
+        sfl->prev = shd_hdr.tail;
+        shd_hdr.tail->next = sfl;
+    }
+    shd_hdr.tail = sfl;
+}
+void remove_shared_file_list(struct shared_file_list* sfl) {
+    if (sfl->prev) {
+        sfl->prev->next = sfl->next;
+        if (sfl->next) {
+            sfl->next->prev = sfl->prev;
+        } else {
+            shd_hdr.tail = sfl->prev;
+        }
+    } else {
+        if (sfl->next) {
+            sfl->next->prev = NULL;
+            shd_hdr.head = sfl->next;
+        } else {
+            shd_hdr.head = NULL;
+            shd_hdr.tail = NULL;
+        }
+    }
+    free(sfl->name);
+    free(sfl);
+}
+
 
 void shadow_shared_try_delete(const char* filename, const char * actual_path) {
     // 1. actual_path에 매칭되는 element를 찾아서 있으면 refcnt--
     // 2. refcnt 가 0이면 파일 제거
     // 3. 자료구조는 지워진 element를 가리키는게 없게끔 할수 있으면 만들고 싶은대로 만들면 됨.
+    struct shared_file_list* sfl = find_shared_file(filename);
+    if(sfl) {
+        remove_shared_file_list(sfl);
+    }
 }
 
 void shadow_shared_try_create(const char * filename, char* source, long int size, char** actual_path) {
@@ -87,6 +150,11 @@ void shadow_shared_try_create(const char * filename, char* source, long int size
     //        i. 매칭되는 애가 있으면 해당 entry의 refcnt++, 해당 entry에 적힌 actual_path를 가져와서 파라미터로 제공받은 actual_path 포인터의포인터로 세팅
     //        ii. 매칭되는 애가 없으면 entry를 주어진 정보를 이용해서 만듦. actual_path도 unique하게 만들어야되고, 해당 entry의 refcnt = 1로 세팅. 만든 entry의 actual_path를 가져와서 파라미터로 제공받은 actual_path로 전달
     //    b. 없으면 list 새로 구성하고 a.ii.과 동일하게 동작
+    struct shared_file_list* sfl = find_shared_file(filename);
+    if(!sfl) {
+        add_shared_file_list(filename);
+    }
+    memcpy(&actual_path, filename, strlen(sfl->name)+1);
 }
 int compare_dat_files(int fileno) {
     printf("compare_dat_files in shadow_interface.c\n");

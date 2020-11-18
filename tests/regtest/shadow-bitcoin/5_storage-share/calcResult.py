071d6bee8d291a4e0eff9d8234c45cddf10c5a41
Hyeojin Jhon <hyojin5658@gmail.com>
2020-11-18 21:50:54 +0900

    add tps calculatelogic in calcResult.py

--- tests/regtest/shadow-bitcoin/5_storage-share/calcResult.py
+++ tests/regtest/shadow-bitcoin/5_storage-share/calcResult.py
@@ -13,6 +13,7 @@ def calc_Block_Num(num) :
 		if not file : 
 			print("error! file is not open")
 		data=file.readlines()
+		print("data=",data[3])
 		dict=json.loads(data[3])
 		totalNum += dict["result"]["blocks"]
 		file.close()
@@ -20,11 +21,28 @@ def calc_Block_Num(num) :
 	blockNum=totalNum/num
 	return blockNum
 
+def calc_Tx_Num(num) :
+	totalNum=0
+	for i in range(0,num):
+		path="./datadir/hosts/client" + str(i) + "/stdout-client"+str(i)+ ".client."+str(num+i)+".log"
+		file=open(path,"r")
+		if not file :
+			print("error! file is not open")
+		data=file.readlines()
+		print(data[4])
+		dict=json.loads(data[4])
+		print("txcount = ",dict["txCount"])
+		totalNum += dict["txCount"]
+		file.close()
 
-def writetToFile(nodenum,avgBlockNum,simultime):
+	txNum=totalNum/num
+	return txNum
 
-	bps = avgBlockNum/float(simultime)
-	tpb = float(simultime)/avgBlockNum
+
+def writetToFile(nodenum,avgBlockNum,txNum,simultime):
+
+	bps = float(avgBlockNum)/float(simultime)
+	tps = float(txNum)/float(simultime)
 
 	path = "./datadir/result_node"+str(nodenum)+"_"+str(simultime)+".log"
 	file = open(path,"w")
@@ -33,11 +51,13 @@ def writetToFile(nodenum,avgBlockNum,simultime):
 	file.write(str_write)
 	str_write = "BlockNum = %d\n"% avgBlockNum
 	file.write(str_write)
+	str_write = "TxNum = %d\n"% txNum
+	file.write(str_write)
 	str_write = "Simultime = %d\n" % simultime
 	file.write(str_write)
-	str_write = "BPS(Block per Second) = %f/s \n" % bps
+	str_write = "TPS(Transaction per Second = %f\n" % tps
 	file.write(str_write)
-	str_write = "TPB(Time per Block) = %f/s \n" % tpb
+	str_write = "BPS(Block per Second) = %f/s \n" % bps
 	file.write(str_write)
 	file.close()
 
@@ -46,5 +66,6 @@ if __name__ == '__main__':
 	nodenum = int(sys.argv[1])
 	simultime = int(sys.argv[2])
 	avgBlockNum = calc_Block_Num(nodenum)
+	avgTxNum = calc_Tx_Num(nodenum)
 
-	writetToFile(nodenum,avgBlockNum,simultime)
+	writetToFile(nodenum, avgBlockNum, avgTxNum, simultime)

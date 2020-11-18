db9db83519681e0271da12153c9ba418d359f0cd
Hyeojin Jhon <hyojin5658@gmail.com>
2020-11-18 21:49:55 +0900

    add addexam.sh parameter for transaction xml file

--- tests/regtest/shadow-bitcoin/5_storage-share/addexam.sh
+++ tests/regtest/shadow-bitcoin/5_storage-share/addexam.sh
@@ -1,5 +1,7 @@
+# sh addexam.sh [node no.] [ simultime] [storageshare(disable/disable] [algorithm(pow/coinflip)] [transatcion/normal]
+
 #!/bin/bash
 
 bash cleanup_data.sh $1
-python make_approximate_setmining_test.py transaction $1 $2 $3 $4
+python make_approximate_setmining_test.py $5 $1 $2 $3 $4
 mv base$1N$2T.xml target_example.xml

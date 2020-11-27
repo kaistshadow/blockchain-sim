625720a175fdf880b80ead6cb4d47bb40918570b
Hyunjin Kim <ouiqdmw17@gmail.com>
2020-11-27 15:59:21 +0900

    add instruction on xml creation script on bitcoin storage share regtest

--- tests/regtest/shadow-bitcoin/5_storage-share/addexam.sh
+++ tests/regtest/shadow-bitcoin/5_storage-share/addexam.sh
@@ -1,6 +1,11 @@
-# sh addexam.sh [node no.] [ simultime] [storageshare(disable/disable] [algorithm(pow/coinflip)] [transatcion/normal]
+# sh addexam.sh [node no.] [ simultime] [storageshare(disable/enable] [algorithm(pow/coinflip)] [transaction/normal]
 
 #!/bin/bash
+
+if [ $# -ne 5 ]; then
+ echo "Usage: $0 <node #> <simul. time> <storageshare(disable/enable)> <PoW abs. (pow/coinflip)> <tx inject(transaction/normal)>"
+ exit -1
+fi
 
 bash cleanup_data.sh $1
 python make_approximate_setmining_test.py $5 $1 $2 $3 $4

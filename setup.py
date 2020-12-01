6d842e4995346214ad6d4f0b8957eaa2be8fef4c
Yonggon Kim <ygnkim@kaist.ac.kr>
2020-12-01 16:31:02 +0900

    Revert "Merge branch 'master' into yg-pr-shadow-churn"
    This reverts commit d9541943ed42fd7560870be658c1f378dea1d807, reversing
    changes made to 93eba11e7131da9fa254c6002ba8ca813049e91f.

--- setup.py
+++ setup.py
@@ -44,13 +44,6 @@ def prepare_nodejs():
 def prepare_monero_dependencies():
     exec_shell_cmd("sudo apt-get install build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev libunbound-dev libsodium-dev libpgm-dev ")
 
-def prepare_zcash_dependencies():
-    exec_shell_cmd("sudo apt-get install \
-                    build-essential pkg-config libc6-dev m4 g++-multilib \
-                    autoconf libtool ncurses-dev unzip git python3 python3-zmq \
-                    zlib1g-dev curl bsdmainutils automake libtinfo5")
-
-
 def prepare_rust():
     exec_shell_cmd("sudo apt-get install -y rustc")
 
@@ -84,161 +77,66 @@ def prepare_shadow_dependencies():
     # install dependencies for zeromq (zeromq is used for IPC implementation)
     exec_shell_cmd("sudo apt-get install -y libzmq3-dev")
 
-def process_ENV():
-    rcFile = os.path.expanduser("~/.bashrc")
-    f = open(rcFile, 'r')
-    shadowPath = "export PATH=$PATH:%s" % os.path.abspath("./Install/bin" )
-    libPath = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s" % os.path.abspath("./Install")
-    needWritePath = True
-    needWriteLibPath = True
-    for line in f:
-        if shadowPath in line:
-            needWritePath = False
-        if libPath in line:
-            needWriteLibPath = False
-    if needWritePath:
-        exec_shell_cmd("echo '%s' >> ~/.bashrc && . ~/.bashrc" % shadowPath)
-    if needWriteLibPath:
-        exec_shell_cmd("echo '%s' >> ~/.bashrc && . ~/.bashrc" % libPath)
-
-    print "After installing, execute following commands on your bash. (type without dollor sign)"
-    print "$ source ~/.bashrc"
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script for installation and simulation')
-    parser.add_argument("--all", action="store_true", help="Install the shadow simulator and BLEEP")
+    parser.add_argument("--install", action="store_true", help="Install the shadow simulator and BLEEP")
     parser.add_argument("--test", action="store_true", help="Run tests")
     parser.add_argument("--debug", action="store_true", help="Include debug symbols for shadow")
-    parser.add_argument("--zcash", action="store_true", help="only Zcash build")
-    parser.add_argument("--bitcoin", action="store_true", help="only bitcoin build")
-    parser.add_argument("--monero", action="store_true", help="only monero build")
-    parser.add_argument("--git", action="store_true", help="Run on Git action")
-    parser.add_argument("--litecoin", action="store_true", help="only litecoin build")
-    
+
     args = parser.parse_args()
-    OPT_ALL = args.all
+    OPT_INSTALL = args.install
     OPT_TEST = args.test
     OPT_DEBUG = args.debug
-   
-    OPT_BITCOIN = args.bitcoin
-    OPT_ZCASH = args.zcash
-    OPT_MONERO = args.monero
-    OPT_LITECOIN = args.litecoin
-    OPT_GIT = args.git
-    cmake_bleeplib_opt = "-DBLEEPLIB_OPT=ON"
-    cmake_debug_opt = "-DSHADOW_DEBUG=ON -DBLEEP_DEBUG=ON"
 
-    #default : Bitcoin build
     if len(sys.argv) == 1:
-        exec_shell_cmd("git submodule update --init")
-        #bitcoin dependency
-        exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
-        exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
-        exec_shell_cmd("sudo apt-get install -y libssl-dev")
-        prepare_shadow()
-        prepare_shadow_dependencies()
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s ../; cmake --build . --target install -- -j 8; cd ..;" %(cmake_debug_opt, cmake_bleeplib_opt))
-        process_ENV()   
+        parser.print_help(sys.stderr)
+        sys.exit(1)        
 
+    cmake_debug_opt = "-DSHADOW_DEBUG=ON -DBLEEP_DEBUG=ON"
     if OPT_DEBUG:
         cmake_debug_opt = "-DSHADOW_DEBUG=ON -DBLEEP_DEBUG=ON"
 
-    if OPT_BITCOIN:
-        exec_shell_cmd("git submodule update --init")
-        #bitcoin dependency
-        exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
-        exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
-        exec_shell_cmd("sudo apt-get install -y libssl-dev")
-        prepare_shadow()
-        prepare_shadow_dependencies()
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s ../; cmake --build . --target install -- -j 8; cd ..;" %(cmake_debug_opt, cmake_bleeplib_opt))
-        process_ENV()   
-
-    if OPT_ZCASH:
-        exec_shell_cmd("git submodule update --init")
-        prepare_shadow()
-        prepare_shadow_dependencies()
-        prepare_zcash_dependencies()
-        exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
-        cmake_zcash_opt = "-DZCASH_OPT=ON"
-        cmake_bleeplib_opt = "-DBLEEPLIB_OPT=OFF"
-        cmake_bitcoin_opt = "-DBITCOIN_OPT=OFF"
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s %s %s ../; cmake --build . --target install -- -j 8; cd ..;" %(cmake_debug_opt, cmake_zcash_opt,cmake_bleeplib_opt, cmake_bitcoin_opt))
-        process_ENV()
-
-    if OPT_MONERO:
-        exec_shell_cmd("git submodule update --init")
-        prepare_shadow()
-        prepare_shadow_dependencies()
-        prepare_monero_dependencies()
-        exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
-        cmake_monero_opt = "-DMONERO_OPT=ON"
-        cmake_bleeplib_opt = "-DBLEEPLIB_OPT=OFF"
-        cmake_bitcoin_opt = "-DBITCOIN_OPT=OFF"
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s %s %s ../; cmake --build . --target install -- -j 8; cd ..;" %(cmake_debug_opt, cmake_monero_opt, cmake_bleeplib_opt,cmake_bitcoin_opt))
-        process_ENV()
-
-    if OPT_LITECOIN:
-        exec_shell_cmd("git submodule update --init")
-        #bitcoin dependency
-        exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
-        exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
-        exec_shell_cmd("sudo apt-get install -y libssl-dev")
-        prepare_shadow()
-        prepare_shadow_dependencies()
-        cmake_litecoin_opt = "-DLITECOIN_OPT=ON"
-        cmake_bleeplib_opt = "-DBLEEPLIB_OPT=OFF"
-        cmake_bitcoin_opt = "-DBITCOIN_OPT=OFF"
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s %s %s ../; cmake --build . --target install -- -j 8; cd ..;" %(cmake_debug_opt, cmake_litecoin_opt, cmake_bleeplib_opt,cmake_bitcoin_opt))
-        process_ENV()          
-
-    if OPT_ALL:
+    if OPT_INSTALL:
         # cloning shadow repository (submodule)
         exec_shell_cmd("git submodule update --init")
+
         prepare_shadow()
-        #prepare_nodejs()
+#         prepare_nodejs()
         prepare_rust()
         prepare_golang()
-        prepare_zcash_dependencies()
         prepare_shadow_dependencies()
         prepare_monero_dependencies()
 
-        # ## install boost-lib
+        ## install boost-lib
         exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
 
         ## install bitcoin dependencies
         exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
         ## bitcoin first run (without wallet enabled) dependencies
         exec_shell_cmd("sudo apt-get install -y libssl-dev")
-        cmake_all_opt = "-DALL_OPT=ON"
 
         ## install
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s %s ../; cmake --build . --target install -- -j 8; cd ..;" % (cmake_debug_opt, cmake_all_opt, cmake_bleeplib_opt))
-        process_ENV()
-    
-    if OPT_GIT:
-         # cloning shadow repository (submodule)
-        exec_shell_cmd("git submodule update --init")
-        prepare_shadow()
-        #prepare_nodejs()
-        prepare_rust()
-        prepare_golang()
-        prepare_zcash_dependencies()
-        prepare_shadow_dependencies()
-        prepare_monero_dependencies()
+        exec_shell_cmd("mkdir build; cd build; cmake %s ../; cmake --build . --target install -- -j 8; cd ..;" % cmake_debug_opt)
 
-        # ## install boost-lib
-        exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
 
-        ## install bitcoin dependencies
-        exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
-        ## bitcoin first run (without wallet enabled) dependencies
-        exec_shell_cmd("sudo apt-get install -y libssl-dev")
-        cmake_git_opt = "-DGIT_OPT=ON"
+        rcFile = os.path.expanduser("~/.bashrc")
+        f = open(rcFile, 'r')
+        shadowPath = "export PATH=$PATH:%s" % os.path.abspath("./Install/bin" )
+        libPath = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s" % os.path.abspath("./Install")
+        needWritePath = True
+        needWriteLibPath = True
+        for line in f:
+            if shadowPath in line:
+                needWritePath = False
+            if libPath in line:
+                needWriteLibPath = False
+        if needWritePath:
+            exec_shell_cmd("echo '%s' >> ~/.bashrc && . ~/.bashrc" % shadowPath)
+        if needWriteLibPath:
+            exec_shell_cmd("echo '%s' >> ~/.bashrc && . ~/.bashrc" % libPath)
 
-        ## install
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s %s ../; cmake --build . --target install -- -j 8; cd ..;" % (cmake_debug_opt, cmake_git_opt, cmake_bleeplib_opt))
-        process_ENV()
+        print "After installing, execute following commands on your bash. (type without dollor sign)"
+        print "$ source ~/.bashrc"
 
     if OPT_TEST:
         exec_shell_cmd("mkdir -p build; cd build; cmake ../; make -j8; make test")

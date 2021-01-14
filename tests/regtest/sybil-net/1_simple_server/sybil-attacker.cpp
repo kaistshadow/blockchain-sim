226646bd45161bf30a4472ef8b81258103103f64
Yonggon Kim <ygnkim@kaist.ac.kr>
2021-01-14 18:45:29 +0900

    Implement simple sybil attacker that establishes multiple socket connections to the server

--- tests/regtest/sybil-net/1_simple_server/sybil-attacker.cpp
+++ tests/regtest/sybil-net/1_simple_server/sybil-attacker.cpp
@@ -25,16 +25,21 @@ typedef unsigned int SOCKET;
 #define INVALID_SOCKET      (SOCKET)(~0)
 #define SOCKET_ERROR        -1
 
+class TempMSG {
+public:
+    TempMSG() {}
+};
+
 template<typename MSG>
 class SocketControlStruct {
 private:
     SOCKET _socketfd;
     size_t _offset;
     bool fInbound;
-    std::unique_ptr<ev::io> _connsocket_watcher;
-    std::unique_ptr<ev::io> _datasocket_watcher;
-    std::list<MSG> vRecvMsg;
-    std::list<MSG> vProcessMsg;
+    std::unique_ptr <ev::io> _connsocket_watcher;
+    std::unique_ptr <ev::io> _datasocket_watcher;
+    std::list <MSG> vRecvMsg;
+    std::list <MSG> vProcessMsg;
     std::deque<std::vector<unsigned char>> vSendMsg;
 
 public:
@@ -96,13 +101,7 @@ std::string string_to_hex(const std::string &input) {
     return output;
 }
 
-
-template<typename MSG,
-        bool (*ReceiveMSG)(const char *, unsigned int, std::list<MSG> &, std::list<MSG> &),
-        bool (*ProcessMSG)(MSG &, bool, std::deque<std::vector<unsigned char>> &, std::string, uint16_t, std::string,
-                           bool),
-        bool (*ForgeAddrMSG)(std::vector<std::string>, std::deque<std::vector<unsigned char>> &, std::string, uint16_t),
-        bool (*InitProtocol)(std::deque<std::vector<unsigned char>> &, std::string, uint16_t)>
+template<typename MSG>
 class ActiveNode {
 private:
     ev::io _listen_watcher; // assume a single listening socket per Node
@@ -118,6 +117,9 @@ private:
         if (sock_fd != -1) {
             fcntl(sock_fd, F_SETFL, O_NONBLOCK);
 
+            std::cout << "server: got connection from " << inet_ntoa(their_addr.sin_addr) << ":"
+                      << ntohs(their_addr.sin_port) << "\n";
+/*
             // Create DataSocket and start a event watcher for the DataSocket
             mSocketControl.try_emplace(sock_fd, SocketControlStruct<MSG>(sock_fd, inet_ntoa(their_addr.sin_addr),
                                                                          ntohs(their_addr.sin_port)));
@@ -127,116 +129,12 @@ private:
             ev::io &watcher = mSocketControl[sock_fd].getDataSocketWatcher();
             watcher.set<ActiveNode, &ActiveNode::_dataSocketIOCallback>(this);
             watcher.start(sock_fd, ev::READ);
+*/
         } else {
             if (errno != EAGAIN && errno != EWOULDBLOCK) {
                 std::cout << "accept() failed errno=" << errno << strerror(errno) << "\n";
                 exit(-1);
             }
-        }
-    }
-
-    void _dataSocketIOCallback(ev::io &w, int revents) {
-        std::cout << "data socket IO callback called!" << "\n";
-
-        int datasock_fd = w.fd;
-        auto &socketControl = mSocketControl[datasock_fd];
-        std::list<MSG> &vRecvMsg = socketControl.getVRecvMsg();
-        std::list<MSG> &vProcessMsg = socketControl.getVProcessMsg();
-        std::deque<std::vector<unsigned char>> &vSendMsg = socketControl.getVSendMsg();
-
-        if (revents & EV_READ) {
-            std::cout << "DoRecv!" << "\n";
-
-            // recv from socket, and append received data into the buffer.
-            // typical socket buffer is 8K-64K
-            char pchBuf[0x10000];
-
-            int nBytes = 0;
-
-            nBytes = recv(datasock_fd, pchBuf, sizeof(pchBuf), MSG_DONTWAIT); // What is the meaning of MSG_DONTWAIT?
-            if (nBytes > 0) {
-                std::string recv_str(pchBuf, nBytes);
-                std::cout << "received data(" << _shadow_ip << "):[" << recv_str << "]" << "\n";
-                std::cout << "received data(" << _shadow_ip << ") hex:[" << string_to_hex(recv_str) << "]" << "\n";
-
-                bool ret = ReceiveMSG(pchBuf, nBytes, vRecvMsg, vProcessMsg);
-                if (!ret) {
-                    std::cout << "error while BitcoinReceiveMsg" << "\n";
-                    exit(-1);
-                }
-
-                while (!vProcessMsg.empty()) {
-                    // Just take one message
-                    MSG &msg = vProcessMsg.front();
-
-                    // process message
-                    ret = ProcessMSG(msg, socketControl.isInboundSocket(), vSendMsg, socketControl.their_ip,
-                                     socketControl.their_port, _shadow_ip, false);
-                    if (!ret) {
-                        std::cout << "error while processing message" << "\n";
-                        exit(-1);
-                    }
-                    if (!vSendMsg.empty()) // set Writable
-                        socketControl.getDataSocketWatcher().set(datasock_fd, ev::READ | ev::WRITE);
-
-                    vProcessMsg.pop_front();
-                }
-
-            } else if (nBytes == 0) {
-                std::cout << "connection closed while recv" << "\n";
-                // delete this; // is it okay for this self-destruction? // TODO : it's not okay for multiple data sockets
-                close(w.fd);
-                mSocketControl.erase(w.fd);
-            } else if (nBytes < 0) {
-                // error
-                std::cout << "Error while recv" << "\n";
-            }
-        } else if (revents & EV_WRITE) {
-            auto it = vSendMsg.begin();
-            size_t nSentSize = 0;
-
-            size_t nSendOffset = socketControl.getSendOffset();
-
-            while (it != vSendMsg.end()) {
-                const auto &data = *it;
-                assert(data.size() > nSendOffset);
-                int nBytes = 0;
-                {
-                    nBytes = send(datasock_fd, reinterpret_cast<const char *>(data.data()) + nSendOffset,
-                                  data.size() - nSendOffset, MSG_NOSIGNAL | MSG_DONTWAIT);
-                }
-                if (nBytes > 0) {
-                    nSendOffset += nBytes;
-                    nSentSize += nBytes;
-                    if (nSendOffset == data.size()) {
-                        nSendOffset = 0;
-                        it++;
-                    } else {
-                        // could not send full message; stop sending more
-                        break;
-                    }
-                } else {
-                    if (nBytes < 0) {
-
-                        // error
-                        if (errno != EWOULDBLOCK && errno != EMSGSIZE && errno != EINTR &&
-                            errno != EINPROGRESS) {
-                            std::cout << "failed errno=" << errno << strerror(errno) << "\n";
-                            exit(-1);
-                        }
-                    }
-                    // couldn't send anything at all
-                    break;
-                }
-            }
-            socketControl.setSendOffset(nSendOffset);
-
-            if (it == vSendMsg.end()) {
-                assert(nSendOffset == 0);
-                // unset writable
-                socketControl.getDataSocketWatcher().set(datasock_fd, ev::READ);
-            }
-            vSendMsg.erase(vSendMsg.begin(), it);
         }
     }
 
@@ -268,19 +166,19 @@ private:
             conn_watcher.stop();
 
 
-            ev::io &data_watcher = socketControl.getDataSocketWatcher();
-            data_watcher.set<ActiveNode, &ActiveNode::_dataSocketIOCallback>(this);
-            data_watcher.start(w.fd, ev::READ);
+//            ev::io &data_watcher = socketControl.getDataSocketWatcher();
+//            data_watcher.set<ActiveNode, &ActiveNode::_dataSocketIOCallback>(this);
+//            data_watcher.start(w.fd, ev::READ);
 
-            // send initializing message
-            std::deque<std::vector<unsigned char>> &vSendMsg = socketControl.getVSendMsg();
-            int ret = InitProtocol(vSendMsg, socketControl.their_ip, socketControl.their_port);
-            if (!ret) {
-                std::cout << "error while initializing protocol" << "\n";
-                exit(-1);
-            }
-            if (!vSendMsg.empty()) // set Writable
-                socketControl.getDataSocketWatcher().set(w.fd, ev::READ | ev::WRITE);
+//            // send initializing message
+//            std::deque<std::vector<unsigned char>> &vSendMsg = socketControl.getVSendMsg();
+//            int ret = InitProtocol(vSendMsg, socketControl.their_ip, socketControl.their_port);
+//            if (!ret) {
+//                std::cout << "error while initializing protocol" << "\n";
+//                exit(-1);
+//            }
+//            if (!vSendMsg.empty()) // set Writable
+//                socketControl.getDataSocketWatcher().set(w.fd, ev::READ | ev::WRITE);
         }
     }
 
@@ -372,26 +270,18 @@ public:
         _listen_watcher.start(_listen_sockfd, ev::READ);
     }
 
-    void SendAddr(std::vector<std::string> vIP) {
-        // get victim address
-        if (mSocketControl.empty())
-            return;
+    ActiveNode(ActiveNode &&rhs) noexcept: _listen_sockfd(std::move(rhs._listen_sockfd)),
+                                           _shadow_ip(std::move(rhs._shadow_ip)),
+                                           mSocketControl(std::move(rhs.mSocketControl)) {
+        rhs._listen_watcher.stop();
+        _listen_watcher.set<ActiveNode, &ActiveNode::_listenSocketIOCallback>(this);
+        _listen_watcher.start(_listen_sockfd, ev::READ);
 
-        // retrieve first data socket, since it is assumed to be an attacker node.
-        // TODO : what if it has multiple connection? How can it recognize the victim node?
-        auto&[fd, socketControl] = *mSocketControl.begin();
-        std::deque<std::vector<unsigned char>> &vSendMsg = socketControl.getVSendMsg();
-        std::cout << "before ForgeAddrMsg" << "\n";
-        bool ret = ForgeAddrMSG(vIP, vSendMsg, socketControl.their_ip, socketControl.their_port);
-        std::cout << "after ForgeAddrMsg" << "\n";
-        if (!ret) {
-            std::cout << "failed to forge ADDR msg" << "\n";
-            exit(-1);
-        }
-        if (!vSendMsg.empty()) { // set Writable
-            socketControl.getDataSocketWatcher().set(fd, ev::READ | ev::WRITE);
-        }
+        std::cout << "activeNode move constructor!" << "\n";
     }
+
+    ActiveNode(const ActiveNode &rhs) = delete; // since ActiveNode includes watcher
+
 
     void ChurnOut() {
         // close all datasocket
@@ -439,7 +329,7 @@ public:
         }
 
 
-        // Create DataSocket and start a event watcher for the DataSocket
+        // Create socket and start a event watcher for the socket
         mSocketControl.try_emplace(remote_fd, SocketControlStruct<MSG>(remote_fd, inet_ntoa(servaddr.sin_addr),
                                                                        ntohs(servaddr.sin_port), false));
         std::cout << "node: try to connect to " << mSocketControl[remote_fd].their_ip << ":"
@@ -452,9 +342,22 @@ public:
 
 };
 
+#define DEFAULT_SOCKET_PORT 3456
 
 int main(int argc, char *argv[]) {
     std::cout << "sybil attacker launched" << "\n";
+
+    std::vector <ActiveNode<TempMSG>> vActiveNode;
+    vActiveNode.emplace_back("111.0.0.1", DEFAULT_SOCKET_PORT);
+    vActiveNode.emplace_back("111.0.0.2", DEFAULT_SOCKET_PORT);
+    vActiveNode.emplace_back("111.0.0.3", DEFAULT_SOCKET_PORT);
+    vActiveNode.emplace_back("111.0.0.4", DEFAULT_SOCKET_PORT);
+    vActiveNode.emplace_back("111.0.0.5", DEFAULT_SOCKET_PORT);
+
+    for (auto &node : vActiveNode) {
+        node.Connect("1.0.0.1", DEFAULT_SOCKET_PORT);
+    }
+
     return 0;
 
 }

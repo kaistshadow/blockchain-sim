b1944d2709b6aceb6072f1c213ffe6449d6da49f
tkdlqm2 <tkdlqm2@naver.com>
2021-01-21 17:32:15 +0900

    Update test algorithnm (hard coding -> file read)
    Feedback : 사용자 단에서 오버헤드를 줄이기 위한 디자인
    
    Before : test.py 에 filter log list를 하드코딩 식으로 디자인
    After : test.py에서 컨트롤 하지 않고, txt 파일로 컨트롤 할 수 있게함. 즉 filter log list가 담긴 파일을 읽어와 로직을 수행하는 것.

--- tests/regtest/test.py
+++ tests/regtest/test.py
@@ -9,90 +9,119 @@ def exec_shell_cmd(cmd):
         exit(-1)
 
 def test_node_init(node_log, standard_log):
-    f = open(node_log, "r")
     result = []
-
-    while True:
-        line = f.readline()
-        if not line: break
-        for i in range(0,len(standard_log)):
+    for i in range(0,len(standard_log)):
+        f = open(node_log, "r")
+        while True:
+            line = f.readline().rstrip("\n")
+            if not line:
+                f.close()
+                break
             compare = standard_log[i] in line
             if compare == True:
                 result.append(standard_log[i])
+                f.close()
                 break
-
-    f.close()            
-    return result
-
-def test_node_connection(node_log, standard_log):
-    f = open(node_log, "r")
-    while True:
-        line = f.readline()
-        if not line: break
-        if standard_log in line:
-            return True
-    return False
-
-def init_test_start(blockchain_network_init_standard, blockchain_connection_standard, blockchain_network, node_count, abs_path):
-    print("----------------------------------------------------------------------------------")
-    print("                           %s node test" %(blockchain_network))
-    print("----------------------------------------------------------------------------------")
-    return_count = 0
-    for i in range(0,node_count):
-        count = 0
-        print("")
-        print("")
-        print("----------------")
-        print("Node%d start" %(i+1))
-        print("----------------")
-        print("")
-        print("[Node initialization]")
-        print("")
-        target_file = abs_path + "bcdnode" + str(i+1)  + "/stdout-bcdnode" + str(i+1) + ".bitcoind.1000.log"
-        target_result = test_node_init(target_file, blockchain_network_init_standard)
-        for j in range(0,len(blockchain_network_init_standard)):
-            if blockchain_network_init_standard[j] in target_result:
-                print("\t%s\t                  ... Successfully" %(blockchain_network_init_standard[j]))
-                count += 1
-            else:
-                return_count = 1
-                print("\t%s\t                  ... failed" %(blockchain_network_init_standard[j]))
-        print("")
-        print("\t -> result : %d/%d  <-" %(count, len(blockchain_network_init_standard)))  
-        print("")
-        print("[Peer connection]")
-        print("")
-        if test_node_connection(target_file, blockchain_connection_standard) == True:
-            print("\t ... Successfully about connection peer")
-            print("")
-        else:
-            print("\t ... Failed connection peer")
-            print("")
-            return_count = 1
-        print("-----------------------------")
-    if return_count == 1:
-        return -1
-    else:
+    if 1 == len(result)/len(standard_log):
         return 0
+    else:
+        return -1
+
+# Different blockchain platforms have different paths
+# Such as bitcoin dir1/dir2/dir3/data,
+# Litecoin dir1/debug.log data  
+# So must be classified by platform
+# This function returns log data file path + file name 's list
+def set_file_name(blockchain, first_dir_name, second_dir_name, thrid_dir_name, node_count, abs_path):
+
+    return_list = []
+
+    if blockchain == "Bitcoin":
+        for i in range(0,node_count):
+            target_file = abs_path + first_dir_name + str(i+1) +second_dir_name + str(i+1) + thrid_dir_name
+            return_list.append(target_file)
+
+    if blockchain == "Monero":
+        pass
+
+    return return_list
+
+def compare_data(blockchain_standard_list, file_name_list):
+    
+    for i in range(0,len(file_name_list)):
+        for j in range(0,len(blockchain_standard_list)):
+            if test_node_init(file_name_list[i], blockchain_standard_list[j]) == 0:
+                continue
+            else:
+                return -1
+    return 0
+
+def init_test_start(blockchain_standard_list, blockchain_network, node_count, abs_path):
+
+    if blockchain_network == "Bitcoin":
+        file_list = set_file_name("Bitcoin", "bcdnode", "/stdout-bcdnode", ".bitcoind.1000.log", node_count, abs_path)
+    if blockchain_network == "Monero":
+        pass
+
+    return compare_data(blockchain_standard_list, file_list)
+
+
+# n parameter means the number of reading file such as node initialzation standard, peer connection standard, wallet standard etc ... 
+# n == 2 : node initialzation standard, peer connection standard
+# n == 3 : node initialzation standard, peer connection standard, wallet standard 
+def file_read(abs_file_path, n):
+    
+    return_list = []
+    # ---------------------------------------------------------------------- if you want to add some needed to check file, write down like below
+    path_init = abs_file_path + "/initialization.txt"
+    path_peer = abs_file_path + "/peerconnection.txt"
+
+    standard_list = []
+    peer_connection_list = []
+
+    f = open(path_init,"r")
+    while True:
+        line = f.readline().rstrip("\n")
+        standard_list.append(line)
+        if not line: break
+    f.close()
+    return_list.append(standard_list)
+
+    f = open(path_peer, "r")
+    while True:
+        line = f.readline().rstrip("\n")
+        peer_connection_list.append(line)
+        if not line: break
+    f.close()
+    return_list.append(peer_connection_list)
+
+    # ---------- Only bitcoin has wallet enable. so other blockchains can emit this logic. 
+    if n == 3:
+        path_wallet = abs_file_path + "/wallet.txt"
+        wallet_list = []
+        f = open(path_wallet, "r")
+        while True:
+            line = f.readline().rstrip("\n")
+            wallet_list.append(line)
+            if not line: break
+        f.close()
+        return_list.append(wallet_list)
+
+    return return_list
+
 
 def main():
-    # ---------------[ Bitcoin test standard ]-------------------
-
-    bitcoin_init_standard = ['scheduler thread start','torcontrol thread start','Starting network threads','addcon thread start', 'addcon thread start', 'msghand thread start', 'opencon thread start']
-    bitcoin_connection_standard = "New outbound peer connected"
-    # bitcoin_mining_standard = []
-    # bitcoin_block_synchronization = 
-
-    # -------------- [ Litecoin test standard ]-------------------
-
-    litecoin_init_standard = []
-    # bitcoin_connection_standard = []
 
     parser = argparse.ArgumentParser(description='Script for installation and simulation')
+    a = './'
+    abs_path = os.path.abspath(a)
 
     # -------------------------------------------
 
-    parser.add_argument("--bitcoin", action="store_true", help="Install the shadow simulator and BLEEP")
+    parser.add_argument("--bitcoinfirstrun", action="store_true", help="Bitcoin first_run test output log check")
+    parser.add_argument("--bitcoinwallet", action="store_true", help="Bitcoin wallet test output log check")
+
+
     parser.add_argument("--litecoin", action="store_true", help="Install the shadow simulator and BLEEP")
     parser.add_argument("--monero", action="store_true", help="Install the shadow simulator and BLEEP")
     parser.add_argument("--eos", action="store_true", help="Install the shadow simulator and BLEEP")
@@ -104,32 +133,33 @@ def main():
 
     # -------------------------------------------
 
-    OPT_BITCOIN = args.bitcoin
+    OPT_BITCOIN_FIRSTRUN = args.bitcoinfirstrun
+    OPT_BITCOIN_WALLET = args.bitcoinwallet
+
+
     OPT_LITECOIN = args.litecoin
     OPT_MONERO = args.monero
     OPT_EOS = args.eos
     OPT_ETHEREUM = args.ethereum
     OPT_ZCASH = args.zcash
-    OPT_RIPPLE = args.ripple
+    OPT_RIPPLE = args.ripple   
 
     # -------------------------------------------
 
-    cmake_bitcoin_opt = "-BITCOIN_OPT=ON"
-    cmake_litecoin_opt = "-LITECOIN_OPT=ON"
-    cmake_monero_opt = "-MONERO_OPT=ON"
-    cmake_eos_opt = "-EOS_OPT=ON"
-    cmake_ethereum_opt = "-ETHEREUM_OPT=ON"
-    cmake_zcash_opt = "-ZCASH_OPT=ON"
-    cmake_ripple_opt = "-RIPPLE_OPT=ON"
-    
-    # -------------------------------------------
-    a = './'
-    abs_path = os.path.abspath(a)
-    if OPT_BITCOIN:
+    if OPT_BITCOIN_FIRSTRUN:
+        abs_file_path = abs_path + "/shadow-bitcoin/1_firstrun"
+        blockchain_standard_list = file_read(abs_file_path,2)
         abs_path = abs_path + "/shadow-bitcoin/1_firstrun/shadow.data/hosts/"
         node_count = len(os.walk(abs_path).next()[1])
-        return init_test_start(bitcoin_init_standard, bitcoin_connection_standard, "Bitcoin", node_count, abs_path)
+        return init_test_start(blockchain_standard_list, "Bitcoin", node_count, abs_path)
     
+    if OPT_BITCOIN_WALLET:
+        abs_file_path = abs_path + "/shadow-bitcoin/2_with-wallet"
+        blockchain_standard_list = file_read(abs_file_path,3)
+        abs_path = abs_path + "/shadow-bitcoin/2_with-wallet/shadow.data/hosts/"
+        node_count = len(os.walk(abs_path).next()[1])
+        return init_test_start(blockchain_standard_list, "Bitcoin", node_count, abs_path)
+
     if OPT_LITECOIN:
         pass
     

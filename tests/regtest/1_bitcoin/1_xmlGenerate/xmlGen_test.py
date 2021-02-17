1fbf39259c54a465823140a98c1cc837180d205a
tkdlqm2 <tkdlqm2@naver.com>
2021-02-17 19:55:14 +0900

    Add all testcase sub prototypes and xmlGenerator testcase prototype

---
+++ tests/regtest/1_bitcoin/1_xmlGenerate/xmlGen_test.py
@@ -0,0 +1,22 @@
+import os
+from subprocess import check_output
+import argparse
+import sys
+
+def exec_shell_cmd(cmd):
+    if os.system(cmd) != 0:
+        print("error while executing '%s'" % cmd)
+        exit(-1)
+
+def main():
+
+    print("----------------TODO-------------------")
+    print("1. Run cli xmlGenerator.py")
+    path = os.path.abspath(".")
+    target_folder = path + "/shadow.data/hosts/bcdnode0/stdout-bcdnode0.bitcoind.1000.log"
+    if os.path.isfile(target_folder):
+        sys.exit(0)
+    else:
+        sys.exit(1)
+if __name__ == '__main__':
+    main()

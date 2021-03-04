087125330a75c150376bfdb995e49a2eb63fff08
Yonggon Kim <ygnkim@kaist.ac.kr>
2021-03-04 18:10:57 +0900

    Add regtest for churnout of bitcoin (WIP)

---
+++ tests/regtest/2_sybilAPI/2_churnout_with_bitcoin/test_churnout_with_bitcoin.cpp
@@ -0,0 +1,29 @@
+//
+// Created by ilios on 21. 3. 4..
+//
+
+#include <bitcoind.h>
+
+#include "ipdb/SimpleIPDatabase.h"
+#include "nodePrimitives/BitcoinNodePrimitives.h"
+#include "node/BenignNode.h"
+
+using namespace libBLEEP_sybil;
+
+int main() {
+    exported_main(); // initialize bitcoin : This is necessary for using Bitcoin's benign/shadow node
+
+    // Initialize basic class for logging & IP management
+    AttackStat attackStat; // kind of logger
+    SimpleIPDatabase dummyIPDatabase;
+
+    // Initialize single benign node
+    BenignNode<BitcoinNodePrimitives> benignNode(&attackStat, &dummyIPDatabase, "2.0.0.1", 8333);
+    benignNode.SetChurnOutTimer(30);
+
+    // Start attack
+    struct ev_loop *libev_loop = EV_DEFAULT;
+    ev_run(libev_loop, 0);
+    std::cout << "all watchers are removed" << "\n";
+
+}

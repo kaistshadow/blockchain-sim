c1df174aa279cf3152f1217ce452f4a503a80ab1
Yonggon Kim <ygnkim@kaist.ac.kr>
2021-03-10 14:15:09 +0900

    Remove unused files (BLEEPapp, deprecated directory)

--- deprecated/test/BLEEP/test-rc1-gossip-recovery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 1 ]; then
-    n=`expr $1 + 1`
-    python python-script/test-rc1-gossip-recovery.py --nodenum $1
-    python python-script/print_log.py $n rc1-gossip-datadir PEER
-    python python-script/network_graph.py $n
-fi

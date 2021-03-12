31816924e8d78743b81d06dab8c7d697748be3c4
Yonggon Kim <ygnkim@kaist.ac.kr>
2021-03-12 16:51:46 +0900

    Add emulation support for Monero (#241)
    - Import Monero as a submodule
    - Change setup.py to include monero dependencies, update nested submodules (of Monero)
    - Refactoring for add_compile_options of BLEEP
    - Add a shadow-plugin build within Monero CMakeLists.txt
    - Change CMAKE_SOURCE_DIR within Monero in order to make it compatible to BLEEP build
    - Commentify while-loop in perf_timer.cpp (to avoid infinite loop during Shadow emulation)
    - Suppress use of pthread_mutexattr_setpshared in liblmdb of Monero (Shadow doesn't support this syscall)
    - Disable MDB_USE_ROBUST in liblmdb of Monero (to avoid non-supported syscall during shadow emulation)
    - Use old OpenSSL(1.1.0f) for Monero instead of system's OpenSSL library
    - Use newest unbound for Monero (to resolve old OpenSSL-related compilation issue)
    - Add simple regression test for Monero (tests/regtest/4_monero/1_start)

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ def process_ENV():
     print ("After installing, execute following commands on your bash. (type without dollor sign)")
     print ("$ source ~/.bashrc")
 
+def prepare_monero_dependencies():
+    exec_shell_cmd("sudo apt install libminiupnpc-dev")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script for installation and simulation')
@@ -88,35 +90,42 @@ if __name__ == '__main__':
 
 
     if len(sys.argv) == 1:
-        exec_shell_cmd("git submodule update --init")
-        #bitcoin dependency
+        exec_shell_cmd("git submodule update --init --recursive")
+        # bitcoin dependency
         exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
         exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
         exec_shell_cmd("sudo apt-get install -y libssl-dev")
+        # monero dependency
+        prepare_monero_dependencies()
+
         prepare_shadow()
         prepare_shadow_dependencies()
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s ../; cmake --build . --target install -- -j 8; cd ..;" %(cmake_debug_opt, cmake_bleeplib_opt))
+        exec_shell_cmd("mkdir build; cd build; cmake %s %s ../; cmake --build . --target install -- -j 8; cd ..;" % (
+        cmake_debug_opt, cmake_bleeplib_opt))
         process_ENV()   
 
     if OPT_DEBUG:
         cmake_debug_opt = "-DSHADOW_DEBUG=ON -DBLEEP_DEBUG=ON"
 
     if OPT_BITCOIN:
-        exec_shell_cmd("git submodule update --init")
-        #bitcoin dependency
+        exec_shell_cmd("git submodule update --init --recursive")
+        # bitcoin dependency
         exec_shell_cmd("sudo apt-get install -y libboost-all-dev")
         exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
         exec_shell_cmd("sudo apt-get install -y libssl-dev")
-        #rpc client dependency
+        # monero dependency
+        prepare_monero_dependencies()
+        # rpc client dependency
         exec_shell_cmd("sudo apt-get install -y libjsoncpp-dev")
         prepare_shadow()
         prepare_shadow_dependencies()
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s ../; cmake --build . --target install -- -j 8; cd ..;" %(cmake_debug_opt, cmake_bleeplib_opt))
+        exec_shell_cmd("mkdir build; cd build; cmake %s %s ../; cmake --build . --target install -- -j 8; cd ..;" % (
+        cmake_debug_opt, cmake_bleeplib_opt))
         process_ENV()   
 
     if OPT_ALL:
         # cloning shadow repository (submodule)
-        exec_shell_cmd("git submodule update --init")
+        exec_shell_cmd("git submodule update --init --recursive")
         prepare_shadow()
         prepare_shadow_dependencies()
 
@@ -126,18 +135,21 @@ if __name__ == '__main__':
 
         ## install bitcoin dependencies
         exec_shell_cmd("sudo apt-get install -y autoconf libtool libevent-dev libdb++-dev")
+        ## prepare monero dependency
+        prepare_monero_dependencies()
         ## bitcoin first run (without wallet enabled) dependencies
         exec_shell_cmd("sudo apt-get install -y libssl-dev")
         cmake_all_opt = "-DALL_OPT=ON"
 
         ## install
-        exec_shell_cmd("mkdir build; cd build; cmake %s %s %s ../; cmake --build . --target install -- -j 8; cd ..;" % (cmake_debug_opt, cmake_all_opt, cmake_bleeplib_opt))
+        exec_shell_cmd("mkdir build; cd build; cmake %s %s %s ../; cmake --build . --target install -- -j 8; cd ..;" % (
+        cmake_debug_opt, cmake_all_opt, cmake_bleeplib_opt))
         process_ENV()
     
     if OPT_GIT:
          # cloning shadow repository (submodule)
-        exec_shell_cmd("git submodule update --init")
-        prepare_shadow()
+         exec_shell_cmd("git submodule update --init --recursive")
+         prepare_shadow()
         prepare_shadow_dependencies()
         ### Until the complete tests are done, let's exclude following external modules from git all build
 
@@ -160,7 +172,7 @@ if __name__ == '__main__':
         exec_shell_cmd("mkdir -p build; cd build; cmake %s ../; make -j8; make test" %(cmake_test_opt))
 
     if OPT_UNITTEST:
-        exec_shell_cmd("git submodule update --init")
+        exec_shell_cmd("git submodule update --init --recursive")
         cmake_unittest_opt = "-DUNITTEST_OPT=ON"
         exec_shell_cmd("mkdir -p build; cd build; cmake %s ../; make -j8" %(cmake_unittest_opt))
 

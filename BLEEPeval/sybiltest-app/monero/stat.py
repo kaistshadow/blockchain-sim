608e3d35644423d64e5d303ad5a532ce21915eed
Yonggon Kim <ygnkim@kaist.ac.kr>
2021-04-08 11:07:05 +0900

    Update x-axis printing of stat.py script

--- BLEEPeval/sybiltest-app/monero/stat.py
+++ BLEEPeval/sybiltest-app/monero/stat.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib
 import math
 
 f = open("shadow.data/hosts/tester/stdout-tester.MONEROP2P_TESTER.1000.log", "r")
@@ -38,13 +39,24 @@ for line in f:
         shadowNodeCountdata.append(shadowNodeCount)
 
 ax = plt.gca()
-# x = list(xy.values()[:20])
-# yint = range(min(x), max(x) + 1)
-# plt.yticks(yint)
 plt.xticks(rotation=40)
 formatter = mdates.DateFormatter("%H:%M:%S")
 ax.xaxis.set_major_formatter(formatter)
-ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+
+# ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+ax.xaxis.set_major_locator(mdates.HourLocator(byhour=[0,12]))
+ax.set_xlim(datetime.datetime(2020,1,1), timedata[-1])
+ax.set_ylim([0,max(max(shadowNodeCountdata),max(benignNodeCountdata))])
+
+first_index = 0
+for index, val in enumerate(shadowNodeCountdata):
+    if val > 0:
+        first_index = index
+        break
+locs = [mdates.date2num(timedata[first_index])] + list(ax.get_xticks())
+locator= matplotlib.ticker.FixedLocator(locs)
+ax.xaxis.set_major_locator(locator)
+
 plt.plot(timedata, benignNodeCountdata, label='BenignNode connection')
 plt.plot(timedata, shadowNodeCountdata, label='ShadowNode connection')
 plt.legend()

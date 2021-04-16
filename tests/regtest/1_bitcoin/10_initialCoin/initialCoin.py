892e102aa259382cbc52969fdd91b882b0674545
tkdlqm2 <tkdlqm2@naver.com>
2021-04-16 16:08:36 +0900

    Test git action

--- tests/regtest/1_bitcoin/10_initialCoin/initialCoin.py
+++ tests/regtest/1_bitcoin/10_initialCoin/initialCoin.py
@@ -18,7 +18,6 @@ def main():
     parser.add_argument("--regtest", action="store_true", help="Make xml file.")
     args = parser.parse_args()
     OPT_REGTEST = args.regtest
-    
     difficulty = ""
 
     if OPT_REGTEST:
@@ -29,26 +28,32 @@ def main():
         difficulty = utils.get_difficulty_fromXML(present_path)
 
     # xml 파일 생성 확인
-    target_folder_xml = test_modules.test_xml_existence("output2.xml")
+    target_folder_xml = test_modules.test_xml_existence("output.xml")
 
     # 생성된 xml 파일로 부터 runtime, node_id, plugin 들을 뽑아옴.
     runtime, node_id_list, plugin_list = xml_modules.get_xml_info_new(target_folder_xml)
 
-    # bitcoind data dir 설정 파일 생성.
+    # 지금 예제는 transaction injector를 사용 안하기에 별도의 xml파일을 만들어줌.
+    # 기존 xml에는 transaction.so에 대한 정의가 있어서, 이를 삭제안해주면 shadow가 실행이 안됨.
     target_path = path + "/data"
+    utils.remove_tx_plugin(target_folder_xml)
+    target_folder_xml = target_folder_xml[:len(target_folder_xml)-4] + "2.xml"
+    shadow_command = "shadow " + target_folder_xml
+
     # shadow 실행
     print("shadow running ...")
     utils.subprocess_open('shadow output2.xml > output.txt')
     output_file = path + "/output.txt" 
 
-    # shadow output test
+    # shadow output 파일 존재 검사.
     target_folder_file = test_modules.test_shadow_output_file_existence("regtest", node_id_list)
 
-    # shadow output 파일 검사.
+    # shadow output 파일 내용 검사.
     test_modules.test_shadow(target_folder_file, runtime, node_id_list, output_file)
 
     # shadow plugin의 결과 값 뽑아옴.
     simulation_output_file = test_modules.test_file_existence(node_id_list, plugin_list)
+
 
     # wallet test 시작.
     test_modules.test_walletAddress(simulation_output_file, int(len(plugin_list)/2))

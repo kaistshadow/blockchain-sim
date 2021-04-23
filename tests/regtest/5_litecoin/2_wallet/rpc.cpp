ddafa7faa3af93a1a23ee9d05b8d54e6643f1a0c
tkdlqm2 <tkdlqm2@naver.com>
2021-04-23 14:09:48 +0900

    add wallet regtest

---
+++ tests/regtest/5_litecoin/2_wallet/rpc.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <jsoncpp/json/json.h>
+#include "../../../../testlibs/rpc_client.h"
+
+void rpc_generatetoaddress(std::string wallet, char* ipport) {
+    Json::Value params;
+    params.clear();
+
+    params = Json::arrayValue;
+    params.append(wallet);
+    bitcoin_rpc_request("setgeneratetoaddress",params);
+}
+
+void rpc_getblockchaininfo(){
+    Json::Value params;
+    params.clear();
+    bitcoin_rpc_request("getblockchaininfo",params);
+}
+
+void rpc_getmempoolinfo(){
+    Json::Value params;
+    params.clear();
+    bitcoin_rpc_request("getmempoolinfo",params);
+}
+
+void make100blocks(std::string wallet_address) {
+    Json::Value params;
+    params.append(wallet_address);
+    for (int i = 0; i < 101; i++) {
+        std::cout<<i;
+        bitcoin_rpc_request("setgeneratetoaddressnotime", params);
+        std::cout << "\n";
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // init
+    std::cout<<"start client \n";
+    url = std::string(argv[1]);
+    id_pwd = "a:1234";
+    Json::Value params;
+    params.clear();
+
+    // method 1: generate node's wallet
+    params = Json::arrayValue;
+    bitcoin_rpc_request("getnewaddress", params);
+    std::cout<<"-- wallet --\n";
+
+    std::string wallet = json_resp["result"].asString();
+    std::cout<<wallet<<std::endl;
+
+    return 0;
+}

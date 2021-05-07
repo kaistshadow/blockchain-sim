91b3cab124fde58ecb1d93124d61d364c8921716
Hyeojin Jhon <hyojin5658@gmail.com>
2021-05-07 17:32:44 +0900

    Fix TPS/Latency start shell script (#320)

--- BLEEPeval/tpstest-app/bitcoin/start_test.sh
+++ BLEEPeval/tpstest-app/bitcoin/start_test.sh
@@ -1,5 +1,5 @@
-if [ $# -ne 1 ]; then
-  echo "script need 1 parameter about difficulty "
+if [ $# -ne 2 ]; then
+  echo "script need 2 parameter about difficulty and policy(TPS/Latency) "
   exit 1
 fi
 if [ $1 -gt 3 ]; then
@@ -15,4 +15,13 @@ if [ ! -e $path/coinflip_hash.txt ]; then
 fi
 
 sh clean_data.sh $1
-shadow test-BitcoinP2P.xml > data/output.txt
+
+if [ $2 = "TPS" ]; then
+  shadow test-BitcoinTPS.xml > data/output.txt
+elif [ $2 = "Latency" ]; then
+  shadow test-BitcoinLatency.xml > data/output.txt
+else
+  echo "policy is not exist"
+  exit 1
+fi
+

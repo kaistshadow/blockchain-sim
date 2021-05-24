92bc41336ff78f443684284dafc437698cad123c
Hyunjin Kim <ouiqdmw17@gmail.com>
2021-05-24 10:09:54 +0900

    Fix test script

--- BLEEPemul/emulation/run_test.sh
+++ BLEEPemul/emulation/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ./xmlGenerator.py 1000 90000
+python ./xmlGenerator.py 1000 43200
 sleep 0.2
 rm ../../interfaces/shadow_memshare_interface/shadow_memshare_interface.h
 sleep 0.2
@@ -10,11 +10,11 @@ cd ../..
 python ./setup.py --all
 cd -
 sleep 0.2
-shadow -d datadir -h 10000 -w 8 output.xml
+../../Install/bin/shadow -d datadir -h 10000 -w 8 output.xml
 rm ../../interfaces/shadow_memshare_interface/shadow_memshare_interface.h
 cp ../../../shadow_memshare_interface_disabled.h ../../interfaces/shadow_memshare_interface/shadow_memshare_interface.h
 cd ../..
 python ./setup.py --all
 cd -
 sleep 0.2
-shadow -d datadir -h 10000 -w 8 output.xml
+../../Install/bin/shadow -d datadir -h 10000 -w 8 output.xml
